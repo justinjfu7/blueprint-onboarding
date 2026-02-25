bbbbf6afc1e4f360e334d508262c8473c633c4e2
Justin Fu <jfu7171@berkeley.edu>
2026-02-24 21:37:29 -0800

    [chore]Fixed the layout of everything besides the comments

--- app/page.tsx
+++ app/page.tsx
@@ -22,16 +22,28 @@ export default function Home() {
         </div>
 
         <div className={styles.contentScroll}>
-          <ProfileIcon size={24} />
-          <p>etam3 at Mission Bit</p>
-          <p>San Francisco, CA</p>
+          <div className={styles.postHeader}>
+            <ProfileIcon className={styles.avatar} size={24} />
 
-          <p>
-            Image Link:
-            https://cdn.britannica.com/51/178051-050-3B786A55/San-Francisco.jpg
-          </p>
+            <div className={styles.userBlock}>
+              <div className={styles.userLine}>
+                <span className={styles.username}>etam3</span>
+                <span className={styles.at}> at </span>
+                <span className={styles.org}>Mission Bit</span>
+              </div>
+              <div className={styles.location}>San Francisco, CA</div>
+            </div>
+          </div>
 
-          <p>
+          <div className={styles.imageWrap}>
+            <img
+              src="https://cdn.britannica.com/51/178051-050-3B786A55/San-Francisco.jpg"
+              className={styles.postImage}
+              alt=""
+            />
+          </div>
+
+          <div className={styles.caption}>
             This past weekend, I taught at Mission Bit. I was working with a
             group of high school students who were building their first web
             pages. I really enjoyed being able to help guide 10 students on
@@ -39,20 +51,34 @@ export default function Home() {
             eager to learn, and I&#39;m glad I signed up. Highly recommend to
             any other software engineers interested in volunteering! Sign-up
             here: https://missionbit.org/get-involved/volunteer-with-us/
-          </p>
+          </div>
+          <div className={styles.actions}>
+            <div className={styles.likes}>3 Likes</div>
+            <div className={styles.comments}>View 2 Comments</div>
+          </div>
+          <div className={styles.icons}>
+            <HeartIcon className={styles.heartIcon} size={24} />
+            <TbMessageCircle className={styles.commentIcon} size={24} />
+            <ShareIcon className={styles.shareIcon} size={24} />
+          </div>
+          <div className={styles.datePosted}>
+            <p>February 1</p>
+          </div>
+          <div className={styles.postHeader}>
+            <ProfileIcon className={styles.avatar} size={24} />
 
-          <p>3 Likes</p>
-          <p>View 2 Comments</p>
-          <HeartIcon size={24} />
-          <TbMessageCircle size={24} />
-          <ShareIcon size={24} />
-
-          <p>February 1</p>
-
-          <ProfileIcon size={24} />
-          <p>carolyn123 at Boys and Girls Club</p>
-          <p>Oakland, CA</p>
-          <p>I recently volunteered at my local Boys and Girls Club!</p>
+            <div className={styles.userBlock}>
+              <div className={styles.userLine}>
+                <span className={styles.username}>carolyn123</span>
+                <span className={styles.at}> at </span>
+                <span className={styles.org}>Boys and Girls Club</span>
+              </div>
+              <div className={styles.location}>Oakland, CA</div>
+            </div>
+          </div>
+          <div className={styles.caption}>
+            I recently volunteered at my local Boys and Girls Club!
+          </div>
         </div>
       </div>
     </main>
