6adcf33560a5bd6bf1532862896c9f4057944001
Justin Fu <jfu7171@berkeley.edu>
2026-03-04 23:39:15 -0800

    [feat] Added comments under posts

--- supabase/query.ts
+++ supabase/query.ts
@@ -1,11 +1,21 @@
-import { supabase } from '@/supabase/client';
-import { NoSubstitutionTemplateLiteral } from 'typescript';
+import { CommentDB, PostProps } from "@/components/Post";
+import { supabase } from "@/supabase/client";
 
+export async function getAllPosts(): Promise<PostProps[]> {
+  const { data, error } = await supabase.from("Posts").select("*");
 
-export async function getAllPosts(): Promise<Post[]> {
-	const { data,error } = await supabase.from('Posts').select('*');
-	
-  if (error) throw error
-	
-	return data;
-} 
+  if (error) throw error;
+
+  return data;
+}
+
+export async function getCommentsForPost(postId: number): Promise<CommentDB[]> {
+  const { data, error } = await supabase
+    .from("Comments")
+    .select("id, created_at, post_id, user_name, num_likes, comment_text")
+    .eq("post_id", postId)
+    .order("created_at", { ascending: true });
+
+  if (error) throw error;
+  return data ?? [];
+}
